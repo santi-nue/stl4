b6b879eac3d35394bb80de8241f05a1c9e8f0ab9
santi-nue <68065589+santi-nue@users.noreply.github.com>
2023-03-20 18:20:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,17 @@ Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart
 """
 
 with st.echo():
+    from parsel import Selector
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
     from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
     from selenium.webdriver.common.by import By
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC    
+    
+    
 
     @st.experimental_singleton
     def get_driver():
@@ -32,16 +38,28 @@ with st.echo():
     
     # Maximize the window and let code stall 
     # for 10s to properly maximise the window.
-    driver.maximize_window()
+    # driver.maximize_window()
+    # time.sleep(6)
+    
+    
+    # wait for page to load
+    element = WebDriverWait(driver=driver, timeout=5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div[role=contentinfo]'))
+    )
+    
+    
+    
+    search_box = driver.find_element_by_css_selector('input[aria-label="search"]') 
+    search_box.send_keys(
+        'https://www.planespotters.net/production-list/index'
+    )
+    
+    # either press the enter key
+    search_box.send_keys(Keys.ENTER)
+    # or click search button
+    # search_button = driver.find_element_by_css_selector('button[icon="NavSearch"]')
+    # search_button.click()   
+    
     time.sleep(6)
-    
-    stext = driver.find_element(By.ID, "search")
-    
-    
-    # stext.clear()
-    stext.send_keys("https://www.planespotters.net/production-list/index")
-    
-    time.sleep(6)
-    
 
     st.code(driver.page_source)
