a3af252905ac36099af31d862d1007a4ccdbb0ca
santi-nue <68065589+santi-nue@users.noreply.github.com>
2023-03-20 18:50:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.echo():
     from selenium.webdriver.chrome.options import Options
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC    
-    
+    from selenium.webdriver.common.keys import Keys
     
 
     @st.experimental_singleton
